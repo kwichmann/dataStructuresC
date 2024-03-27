4858085462ffa07cf374111534dbcafce9bcdde0
Kristian Wichmann <kristian.wichmann@gmail.com>
2024-03-27 19:08:36 +0100

    A few more functions

--- stack/stack.h
+++ stack/stack.h
@@ -1,4 +1,5 @@
 # include <stdlib.h>
+# include <stdbool.h>
 const int STACK_SIZE = 16;
 
 struct Stack {
@@ -12,15 +13,27 @@ Stack* newStack() {
     Stack* stack = malloc(sizeof(Stack));
     stack->top = -1;
     return(stack);
-};
+}
+
+void emptyStack(Stack* stack) {
+    stack->top = -1;
+}
+
+bool isEmpty(Stack* stack) {
+    return(stack->top == -1);
+}
+
+bool isFull(Stack* stack) {
+    return(stack->top == STACK_SIZE - 1);
+}
 
 void push(Stack* stack, int item) {
     stack->top++;
     stack->items[stack->top] = item;
-};
+}
 
 int pop(Stack* stack) {
     int out = stack->items[stack->top];
     stack->top--;
     return(out);
-};
+}
