007e0e74747f3dc1ea70f03c12dd2371bf906c30
Kristian Wichmann <kristian.wichmann@gmail.com>
2024-04-14 03:26:40 +0200

    Hash map implemented. Needs cleanup and tests

--- hashMap/hashMap.h
+++ hashMap/hashMap.h
@@ -1,6 +1,10 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
 /*
     fnv1a hashing function as described here:
     https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function#FNV-1a_hash
@@ -18,6 +22,134 @@ unsigned long long int fnv1a(char *data) {
         index++;
     }
     return(hash);
+}
+
+#define LOG2NUMBER_OF_BUCKETS 4
+#define NUMBER_OF_BUCKETS (1 << LOG2NUMBER_OF_BUCKETS)
+#define HASH_MASK (NUMBER_OF_BUCKETS - 1)
+
+struct BucketItem {
+    unsigned long long int hash;
+    int value;
+};
+
+typedef struct BucketItem BucketItem;
+
+// Buckets are dynamic arrays
+#define BUCKET_START_CAPACITY 4
+
+struct Bucket {
+    int capacity;
+    int size;
+    BucketItem *array;
+};
+
+typedef struct Bucket Bucket;
+
+Bucket init_bucket() {
+    Bucket bucket;
+    bucket.capacity = BUCKET_START_CAPACITY;
+    bucket.size = 0;
+    bucket.array = malloc(BUCKET_START_CAPACITY * sizeof(BucketItem));
+    return(bucket);
+}
+
+void show_bucket(Bucket *bucket) {
+    for (int i = 0; i < bucket->size; i++) {
+        printf("\tHash: %llx, Value: %i\n", bucket->array[i].hash, bucket->array[i].value);
+    }
+}
+
+void upsize_bucket(Bucket *bucket) {
+    bucket->capacity *= 2;
+    bucket->array = realloc(bucket->array, bucket->capacity * sizeof(BucketItem));
+}
+
+void downsize_bucket(Bucket *bucket) {
+    bucket->capacity /= 2;
+    bucket->array = realloc(bucket->array, bucket->capacity * sizeof(BucketItem));
+}
+
+void add_to_bucket(Bucket *bucket, unsigned long long int hash, int value) {
+    for (int i = 0; i < bucket->size; i++) {
+        if (bucket->array[i].hash == hash) {
+            bucket->array[i].value = value;
+            return;
+        }
+    }
+    if (bucket->size == bucket->capacity) {
+        upsize_bucket(bucket);
+    }
+    bucket->array[bucket->size].hash = hash;
+    bucket->array[bucket->size++].value = value;
+}
+
+int* lookup_in_bucket(Bucket *bucket, unsigned long long int hash) {
+    static int out; // Will this cause memory leak?
+    for (int i = 0; i < bucket->size; i++) {
+        if (bucket->array[i].hash == hash) {
+            out = bucket->array[i].value;
+            return(&out);
+        }
+    }
+    return(NULL);
+}
+
+int* delete_from_bucket(Bucket *bucket, unsigned long long int hash) {
+    static int out; // Will this cause memory leak?
+    for (int i = 0; i < bucket->size; i++) {
+        if (bucket->array[i].hash == hash) {
+            out = bucket->array[i].value;
+            if (bucket->size < bucket->capacity / 4) {
+                downsize_bucket(bucket);
+            }
+            memmove(&bucket->array[i], &bucket->array[i + 1], (bucket->size - i - 1) * sizeof(BucketItem));
+            bucket->size--;
+            return(&out);
+        }
+    }
+    return(NULL);
+}
+
+struct HashMap {
+    Bucket buckets[NUMBER_OF_BUCKETS];
+};
+
+typedef struct HashMap HashMap;
+
+void init_hashMap(HashMap *hashMap) {
+    for (int i = 0; i < NUMBER_OF_BUCKETS; i++) {
+        hashMap->buckets[i] = init_bucket();
+    }
+}
+
+void show_hashMap(HashMap *hashMap) {
+    for (int i = 0; i < NUMBER_OF_BUCKETS; i++) {
+        printf("Bucket %i:\n", i);
+        show_bucket(&hashMap->buckets[i]);
+    }
+}
+
+void add_item(HashMap *hashMap, char *key, int value) {
+    unsigned long long int hash = fnv1a(key);
+    int bucketNumber = (int) hash & HASH_MASK;
+    add_to_bucket(&(hashMap->buckets[bucketNumber]), hash, value);
+}
+
+int lookup_item(HashMap *hashMap, char *key) {
+    unsigned long long int hash = fnv1a(key);
+    int bucketNumber = (int) hash & HASH_MASK;
+    int* lookup = lookup_in_bucket(&(hashMap->buckets[bucketNumber]), hash);
+    assert(lookup != NULL);
+    return(*lookup);
+}
+
+int delete_item(HashMap *hashMap, char *key) {
+    unsigned long long int hash = fnv1a(key);
+    int bucketNumber = (int) hash & HASH_MASK;
+    int* lookup = delete_from_bucket(&(hashMap->buckets[bucketNumber]), hash);
+    assert(lookup != NULL);
+    return(*lookup);
 }
 
 #endif
--- hashMap/hashMap_demo.c
+++ hashMap/hashMap_demo.c
@@ -4,5 +4,18 @@
 
 int main() {
     test_all();
+
+    HashMap hashMap;
+    init_hashMap(&hashMap);
+    add_item(&hashMap, "yolo", 42);
+    add_item(&hashMap, "b", 69);
+    add_item(&hashMap, "bb", 333);
+    add_item(&hashMap, "bbb", 444);
+    add_item(&hashMap, "bbbb", 555);
+    add_item(&hashMap, "bbbb", 666);
+    printf("%i\n", lookup_item(&hashMap, "bbbb"));
+    delete_item(&hashMap, "bbbb");
+    show_hashMap(&hashMap);
+
     return(0);
 }
